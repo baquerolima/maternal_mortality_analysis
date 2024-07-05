ee75650580adaa7a6dcf80c828637419bd46d770
Alexandre Baquero Lima <baquerolima@gmail.com>
2024-07-04 22:34:21 -0300

    Refactoring urllib3 access to BASE_URL

--- pega_dados.py
+++ pega_dados.py
@@ -1,18 +1,27 @@
 import pandas as pd
 import json
-from urllib import request as rq
 import urllib3
 
-def pega_arquivos(url):
-    u = rq.urlopen(url)
+def pega_urls(url_base):
+    """
+    Função que acessa a BASE_URL do SIM e do SINASC para pegar as url's dos arquivos anuais.
+    """
+    http = urllib3.PoolManager()
+    u = http.request('GET', url_base, preload_content=False)
     j = json.loads(u.read())
     r = j['result']['results'][0]['resources']
     lista = []
     for item in r:
         lista.append((item['name'], item['url']))
+        print(item['name'], item['url'])
     return lista
 
 def gera_arquivo(url, name):
+    """
+    Função que acessa a URL indicada, lê o conteúdo e grava num arquivo .csv
+    TODO:
+    Essa é a função que precisa ser modificada para ler o conteúdo e já gravar num banco de dados.
+    """
     http = urllib3.PoolManager()
     r = http.request('GET', url, preload_content=False)
 
@@ -25,11 +34,9 @@ def gera_arquivo(url, name):
 
 
 BASE_URL = 'http://opendatasus.saude.gov.br/api/3/action/package_search?q='
-sim = BASE_URL + 'SIM'
-sinasc = BASE_URL + 'SINASC'
 
-lista_sim = pega_arquivos(sim)
-lista_sinasc = pega_arquivos(sinasc)
+lista_sim = pega_urls(BASE_URL + 'SIM')
+lista_sinasc = pega_urls(BASE_URL + 'SINASC')
 
 for arquivo in lista_sim:
     print(arquivo[0], arquivo[1])
